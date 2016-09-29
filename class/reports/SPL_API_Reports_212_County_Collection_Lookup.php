d4dfa534697176bd52f765559334a052fd741909
Sean Girard <seangirard@yahoo.com>
2016-09-29 10:31:21 -0700

    212

--- class/reports/SPL_API_Reports_212_County_Collection_Lookup.php
+++ class/reports/SPL_API_Reports_212_County_Collection_Lookup.php
@@ -22,7 +22,7 @@ class SPL_API_Reports_212_County_Collection_Lookup extends SPL_API_Reports {
 	}
 
 	protected function lookupBorrowerName() {
-		return 'joy';
+
 		$this->pdo = new PDO($this->config['api']['connect']
                         ,$this->config['api']['web_user']
                         ,$this->config['api']['web_pass']
@@ -33,7 +33,6 @@ class SPL_API_Reports_212_County_Collection_Lookup extends SPL_API_Reports {
 		$sql = "SELECT
 						*
 						FROM spl_county_collections
-						WHERE name LIKE ".'%'.$this->params['vals']['lastname'].'%'."
             ";
 
     	$result = $this->getQuery($sql, $params);
