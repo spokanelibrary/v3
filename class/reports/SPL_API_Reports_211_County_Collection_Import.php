1df9e2bcd7e07bfd4688e9c382da724dca86a31e
Sean Girard <seangirard@yahoo.com>
2016-10-04 08:37:35 -0700

    211

--- class/reports/SPL_API_Reports_211_County_Collection_Import.php
+++ class/reports/SPL_API_Reports_211_County_Collection_Import.php
@@ -33,6 +33,8 @@ class SPL_API_Reports_211_County_Collection_Lookup extends SPL_API_Reports {
 
     $this->userfile = $this->params['files']['collectionlist'];
 
+    return $this->params;
+
     // Remove last run
     if ( is_file($this->upload.$this->subdir.$this->dataload) ) {
       unlink($this->upload.$this->subdir.$this->dataload);
@@ -75,104 +77,6 @@ class SPL_API_Reports_211_County_Collection_Lookup extends SPL_API_Reports {
     return $result;
   }
 
-
-  /*
-  protected function getAddressData() {
-    $params = array();
-
-    $sql = "EXEC spl_stats_load_adhoc_data";
-    $result = $this->getQuery($sql, $params); 
-
-    if ( 'barcode' == $this->fileInspection['type'] ) {
-      $sql = "SELECT
-              DISTINCT adhoc.dataload
-              ,borrower_barcode.bbarcode
-              ,borrower_address.email_address 
-              ,borrower_address.email_name
-              ,borrower.borrower#
-              ,borrower.location
-              ,borrower.btype
-              ,borrower.n_ckos
-              ,CONVERT(date, 
-                        horizon.dbo.spl_get_datetime_from_epoch(borrower.last_cko_date)
-                      ) AS last_cko_date
-              ,CONVERT(date, 
-                        horizon.dbo.spl_get_datetime_from_epoch(borrower.last_authentication_date)
-                      ) AS last_authentication_date
-
-              
-              ,CONVERT(nvarchar(500), ISNULL(borrower_address.address1,'')) + ' ' + CONVERT(nvarchar(500), ISNULL(borrower_address.address2,'')) AS borrower_address
-              ,LEFT(city_st.descr, LEN(city_st.descr)-4) AS borrower_address_city
-              ,RIGHT(city_st.descr, 2) AS borrower_address_state
-              ,SUBSTRING(borrower_address.postal_code,1,5) AS borrower_address_zip
-
-
-              FROM spl_stats_adhoc_dataload AS adhoc
-              
-              LEFT OUTER JOIN horizon.dbo.borrower_barcode AS borrower_barcode
-                ON borrower_barcode.bbarcode = adhoc.dataload
-              LEFT OUTER JOIN horizon.dbo.borrower AS borrower
-                ON borrower.borrower# = borrower_barcode.borrower#
-              LEFT OUTER JOIN horizon.dbo.borrower_address AS borrower_address
-                ON borrower.borrower# = borrower_address.borrower#
-                AND borrower_address.ord = 0
-              LEFT OUTER JOIN horizon.dbo.city_st as city_st
-                ON borrower_address.city_st = city_st.city_st
-              ";
-      $result = $this->getQuery($sql, $params); 
-    } elseif ( 'email' == $this->fileInspection['type'] ) {
-      $sql = "SELECT
-              DISTINCT adhoc.dataload
-              ,borrower_barcode.bbarcode
-              ,borrower_address.email_address 
-              ,borrower_address.email_name
-              ,borrower.borrower#
-              ,borrower.location
-              ,borrower.btype
-              ,borrower.n_ckos
-              ,CONVERT(date, 
-                        horizon.dbo.spl_get_datetime_from_epoch(borrower.last_cko_date)
-                      ) AS last_cko_date
-              ,CONVERT(date, 
-                        horizon.dbo.spl_get_datetime_from_epoch(borrower.last_authentication_date)
-                      ) AS last_authentication_date
-
-              
-              ,CONVERT(nvarchar(500), ISNULL(borrower_address.address1,'')) + ' ' + CONVERT(nvarchar(500), ISNULL(borrower_address.address2,'')) AS borrower_address
-              ,LEFT(city_st.descr, LEN(city_st.descr)-4) AS borrower_address_city
-              ,RIGHT(city_st.descr, 2) AS borrower_address_state
-              ,SUBSTRING(borrower_address.postal_code,1,5) AS borrower_address_zip
-
-
-              FROM spl_stats_adhoc_dataload AS adhoc
-
-              JOIN horizon.dbo.borrower_address AS borrower_address
-                ON adhoc.dataload = borrower_address.email_address
-                AND borrower_address.ord = 0
-              LEFT OUTER JOIN horizon.dbo.borrower AS borrower
-                ON borrower.borrower# = borrower_address.borrower#
-              LEFT OUTER JOIN horizon.dbo.borrower_barcode AS borrower_barcode
-                ON borrower_barcode.borrower# = borrower.borrower#
-              LEFT OUTER JOIN horizon.dbo.city_st as city_st
-                ON borrower_address.city_st = city_st.city_st
-              ";
-      $result = $this->getQuery($sql, $params); 
-    } else {
-      $result = array('error'=>'Unable to match data.');
-    }
-
-    if ( is_file($this->adhoc.$this->subdir.$this->dataload) ) {
-      unlink($this->adhoc.$this->subdir.$this->dataload);
-    }
-
-    if ( is_array($result) && array_key_exists('borrower#', $result[0]) ) {
-      return $result;
-    } else {
-      return false;
-    }
-
-  }
-  */
 
   protected function writeLockFile() {
     $fp = fopen($this->upload.$this->subdir.$this->lockfile, 'w');
