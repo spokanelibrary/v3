09f25413cd4b116490c557c7e392defa5306c697
Sean Girard <seangirard@yahoo.com>
2016-10-07 13:51:10 -0700

    lvl

---
+++ class/reports/SPL_API_Reports_214_Level_Up_Passphrase.php
@@ -0,0 +1,38 @@
+<?php
+
+class SPL_API_Reports_214_Level_Up_Passphrase extends SPL_API_Reports {
+
+	// Make sure class name matches file name!
+	
+	public function getReportData() {
+		//return $this->params['vals']['branch'];
+		
+		$report = new stdClass();
+		//$report->meta = $meta;
+
+		$params = array();
+
+		
+		$sql = "SELECT 
+				ibarcode
+				,substring( processed, 1, 30 ) title
+				,internal_note
+				FROM item_with_title 
+				WHERE internal_note IS NOT NULL
+ 				AND internal_note LIKE 'SS %'
+				ORDER BY ibarcode
+            ";
+
+    	$result = $this->getQuery($sql, $params);
+
+	   	$report->sorted = $result;
+
+    	$report->count = count($result);
+
+	    return $report;
+	}
+
+
+}
+
+?>
